99b7fbdf954fa5c031d588d0660d6605a550ed4e
ashishkumardubey123 <ashish9039062705@gmail.com>
2024-11-24 23:51:45 +0530

    some changes

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -5,16 +5,15 @@ const ownerModel = require("../models/owner-model");
 
 router.get("/", (req, res) => {
   res.send("Hello from owner route");
-  console.log(process.env.NODE_ENV)
 });
 
-// if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV ==='development') {
   router.get("/create", async (req, res) => {
       
       res.send("han me creaeter hun ")
   
   });
-// }
+}
 
 console.log(process.env.NODE_ENV);
 

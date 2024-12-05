385adf133c4f885adf88525d6f93678d852aea6c
ashishkumardubey123 <ashish9039062705@gmail.com>
2024-12-05 15:13:22 +0530

    error resoleve

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -7,13 +7,25 @@ router.get("/", (req, res) => {
   res.send("Hello from owner route");
 });
 
-// if env have some change you need to re satrt the server  
+// if env have some change you need to re satrt the server
 
-if (process.env.NODE_ENV =='development') {
-  router.get("/create", async (req, res) => {
-      
-      res.send("han me creaeter hun ")
-  
+if (process.env.NODE_ENV == "development") {
+  router.post("/create", async (req, res) => {
+    let owner = await ownerModel.find();
+    if (owner.length > 0) {
+      return res.status(500).send("owner already register " );
+      console.log(owner)
+       
+    }
+  let { fullname, email,password}= req.body
+
+  let creaetdOwner= await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+
+    res.send(creaetdOwner);
   });
 }
 
